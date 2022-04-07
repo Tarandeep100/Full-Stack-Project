36a6af5f77edc54294e41a247f27fe2667f96862
unknown <tarandeepsingh100@gmail.com>
2022-04-07 00:50:29 -0400

    updated the html

--- public/server.js
+++ public/server.js
@@ -1,11 +1,6 @@
 var socket = io();
 let userName, room;
-// let userName = prompt("whats your name");
-// let room = prompt("room name");
-// let currentTime = 
 let chat = "";
-// let userName = "taran";
-// let room = "room1";
 let ID = "";
 var form = document.getElementById('form');
 var input = document.getElementById('input');
@@ -17,27 +12,15 @@ socket.on('send data', (data) => {
   // console.log("my id:", ID);
   // console.log(data);
   displayData = {
-    // data: { value: `${userName} has joined the chat`,
-    //         user: "Bot",
-    //         time: new Date().toISOString(),
-    //       },
-    // id: socket.id
     value: `${userName} has joined the chat`,
     user: "Bot",
     time: new Date().toISOString(),
   }
   socket.emit('chat message', displayData);
-  // document.getElementById("room").innerHTML = 'Welcome to ' + room + ' chatroom';
-
 });
 
 // console.log("currentTime->", currentTime);
-// socket.emit("join room", { username: userName, roomName: room });
 socket.emit("join room");
-// socket.on("joining room", (data) => {
-//   // document.getElementById("event").innerHTML = data.username + ' has joined the chat!';
-// })
-
 
 
 
@@ -63,11 +46,6 @@ socket.on("chat message", (data) => {
 
 socket.on("disconnected", (data) => {
   displayData = {
-    // data: { value: `${userName} has joined the chat`,
-    //         user: "Bot",
-    //         time: new Date().toISOString(),
-    //       },
-    // id: socket.id
     value: `${data.user} has left the chat`,
     user: "Bot",
     time: new Date().toISOString(),
@@ -75,8 +53,6 @@ socket.on("disconnected", (data) => {
   socket.emit('chat message', displayData);
   // console.log("disconnected user ->", data.user);
   // console.log("disconnected user ->", userName);
-  // document.getElementById("event").innerHTML = data.user + ' has left the chat!';
-
 })
 
 function displayMessage(data) {
